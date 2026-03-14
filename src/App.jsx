2bf94cc93199ede2cfa7b6485793e7aafa3ee21b
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-03-14 16:44:24 +0100

    remove loader from App and DefaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Homepage from "./pages/Homepage";
 import Cameras from "./pages/Cameras";
 import Lenses from "./pages/Lenses";
 import AboutUs from "./pages/AboutUs";
-import { LoaderProvider } from "./contexts/LoaderContext";
 import CameraDetail from "./pages/CameraDetail";
 import LensDetail from "./pages/LensDetail";
 import Photographers from "./pages/Photographers";
@@ -12,22 +11,20 @@ import PhotographerDetail from "./pages/PhotographerDetail";
 
 function App() {
   return (
-    <LoaderProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route Component={DefaultLayout}>
-            <Route path="/" Component={Homepage} />
-            <Route path="/cameras" Component={Cameras} />
-            <Route path="/cameras/:id" Component={CameraDetail} />
-            <Route path="/lenses" Component={Lenses} />
-            <Route path="/lenses/:id" Component={LensDetail} />
-            <Route path="/photographers" Component={Photographers} />
-            <Route path="/photographers/:id" Component={PhotographerDetail} />
-            <Route path="/about" Component={AboutUs} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </LoaderProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route path="/" Component={Homepage} />
+          <Route path="/cameras" Component={Cameras} />
+          <Route path="/cameras/:id" Component={CameraDetail} />
+          <Route path="/lenses" Component={Lenses} />
+          <Route path="/lenses/:id" Component={LensDetail} />
+          <Route path="/photographers" Component={Photographers} />
+          <Route path="/photographers/:id" Component={PhotographerDetail} />
+          <Route path="/about" Component={AboutUs} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,16 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { useLoader } from "../contexts/LoaderContext";
-import Loader from "../components/Loader";
 
 export default function DefaultLayout() {
-  const { loader } = useLoader();
-
   return (
     <>
       <Header />
-      {loader && <Loader />}
       <main>
         <Outlet />
       </main>
